640ca44bd157dfaece8ebff67eb852989475e5d6
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2024-04-02 18:33:20 +0300

    security was added

--- src/main/java/com/example/restapi/config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.restapi.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(IssueConf.class)
-public class ApplicationConfig {
-}
